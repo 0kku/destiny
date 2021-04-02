6526194d178104d054e0fbe3e7c21dbf900b5544
0kku <40242277+0kku@users.noreply.github.com>
2021-04-03 00:29:08 +0300

    chore: cleanup & update deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
   ],
   extends: [
     "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
   ],
