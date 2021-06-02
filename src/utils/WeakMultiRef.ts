65564f73989f858168bbe677213ef7490c04b82b
0kku <40242277+0kku@users.noreply.github.com>
2021-06-02 06:53:55 +0300

    feat: WeakMultiRef

---
+++ src/utils/WeakMultiRef.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/ban-types */
+
+const refs = new Set<object>();
+
+export class WeakMultiRef {
+  static #cleanup = (
+    { set, ref, target }: {
+      set: Set<WeakRef<object>>,
+      ref: WeakRef<object>,
+      target: WeakMultiRef,
+    },
+  ): void => {
+    set.delete(ref);
+    if (!set.size) {
+      refs.delete(target);
+    }
+  };
+
+  weakKeys = new Set<WeakRef<object>>();
+  #finalizationGroup = new FinalizationRegistry(WeakMultiRef.#cleanup);
+
+  constructor (
+    keys: ReadonlyArray<object>,
+  ) {
+    keys.forEach(key => {
+      const ref = new WeakRef(key);
+      this.weakKeys.add(ref);
+      this.#finalizationGroup.register(
+        key,
+        {
+          set: this.weakKeys,
+          ref,
+          target: this,
+        },
+        ref,
+      );
+    });
+    
+    refs.add(this);
+  }
+}
