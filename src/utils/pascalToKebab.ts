846bef0bf0a4398537d0d41294ac58ee608c18de
0kku <40242277+0kku@users.noreply.github.com>
2021-06-07 07:17:32 +0300

    fix: incorrect capitalization for output

--- src/utils/pascalToKebab.ts
+++ src/utils/pascalToKebab.ts
@@ -1,8 +1,8 @@
 export function pascalToKebab (
   input: string,
 ): string {
-  return input[0].toLowerCase() + input.slice(1).replace(
+  return `${input[0]}${input.slice(1).replace(
     /([A-Z])/g,
     "-$1",
-  );
+  )}`.toLowerCase();
 }
