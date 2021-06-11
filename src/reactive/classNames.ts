bb9a047ea8e93f86d264c8544623363934bc97e6
0kku <40242277+0kku@users.noreply.github.com>
2021-06-11 07:09:04 +0300

    docs: classNames()

--- src/reactive/classNames.ts
+++ src/reactive/classNames.ts
@@ -1,6 +1,32 @@
 import { computed } from "./computed.js";
 import { ReadonlyReactivePrimitive } from "./ReactivePrimitive.js";
 
+/**
+ * This function can be used to create class name string for one or more class names controlled by static or reactive booleans.
+ * 
+ * Example usage: 
+ * ```js
+ * const foo = reactive(true);
+ * const bar = reactive(false);
+ * 
+ * html`
+ *   <div
+ *     class=${classNames({
+ *       foo, // initially added
+ *       bar, // initially omitted
+ *       baz: true, // will always be added
+ *     })}
+ *   />
+ * `;
+ * ```
+ * and the result will be: 
+ * ```html
+ *   <div class="foo baz" />
+ * ```
+ * 
+ * @param input A record containing boolean values indicating if the key should be added in as a class name
+ * @returns A reactive string that updates when any of the inputs change
+ */
 export function classNames (
   input: Record<string, boolean | ReadonlyReactivePrimitive<boolean>>,
 ): ReadonlyReactivePrimitive<string> {
