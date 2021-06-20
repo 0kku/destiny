7394713b7cf86a134426469b36bdf954aa51db4d
0kku <40242277+0kku@users.noreply.github.com>
2021-06-20 23:07:09 +0300

    feat: support async loaded Components in templates

--- src/componentLogic/DestinyFallback.ts
+++ src/componentLogic/DestinyFallback.ts
@@ -23,13 +23,21 @@ export class DestinyFallback extends Component {
         this.#view.value = fallback;
       }
       const module = await props.get("for");
-      if (typeof module !== "object" || !module) {
-        throw new TypeError(`Invalid type ${describeType(module)} supplied for prop:for`);
-      }
-      const component = (module as {default?: unknown}).default;
-      if (!isComponent(component)) {
-        throw new TypeError(`Invalid component constructor ${describeType(component)} supplied for prop:for. Expected type is a module with a Component as the default export.`);
-      }
+      const component = (
+        isComponent(module)
+        ? module
+        : (() => {
+          if (typeof module !== "object" || !module) {
+            throw new TypeError(`Invalid type ${describeType(module)} supplied for prop:for`);
+          }
+          const component = (module as {default?: unknown}).default;
+          if (!isComponent(component)) {
+            throw new TypeError(`Invalid component constructor ${describeType(component)} supplied for asynchronously loaded element. Expected type is a Promise that resolves to a Component or to a module with a Component as the default export.`);
+          }
+          return component;
+        })()
+      );
+
       this.#view.value = xml`
         <${component}
           destiny:ref=${this.forwardProps}
