ef20aca326370b0d07c2423b19e4faf0bf79fb84
Cassandra Choi <nrocinu.lahwran@gmail.com>
2021-06-29 14:19:57 -0400

    fix: typo in `xml` jsdoc (#21)

--- src/parsing/_xml.ts
+++ src/parsing/_xml.ts
@@ -6,7 +6,7 @@ const templateCache = new TemplateCache;
 
 /**
  * Parses an XML template into a `TemplateResult` and hooks up reactivity logic to keep the view synchronized with the state of the reactive items prived in the slots.
- * @param strings The straing parts of the template
+ * @param strings The string parts of the template
  * @param props The slotted values in the template
  */
 export function xml (
