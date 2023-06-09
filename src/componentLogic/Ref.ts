a1fcfb09c8ac5eff23a02baf3e9c88e8161519cd
Okku <40242277+0kku@users.noreply.github.com>
2023-06-09 20:17:17 +0300

    perf: simplify Ref

--- src/componentLogic/Ref.ts
+++ src/componentLogic/Ref.ts
@@ -1,23 +1,15 @@
-export abstract class RefPromise<T> {
-  // The rule is seemingly broken. Return type annotations are not allowed for setters.
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  abstract set value (
-    element: T,
-  );
-
-  abstract then<R> (
-    callbackFn: (value: T) => R,
-  ): RefPromise<R>; 
-}
-
-export class Ref<T extends HTMLElement> {
+export class Ref<T = HTMLElement> {
   #resolve!: (value: T) => void;
   #promise = new Promise<T>(resolve => {
     this.#resolve = resolve;
   });
 
-  constructor () {
-    console.warn("Refs are deprecated and will be removed in v0.8. Use a ReactiveValue instead.");
+  constructor (
+    suppress?: "suppress",
+  ) {
+    if (!suppress) {
+      console.warn("Refs are deprecated and will be removed in a future release. Use a ReactiveValue instead. Pass \"suppress\" to the constructor to suppress this warning.");
+    }
   }
 
   set value (
@@ -28,25 +20,7 @@ export class Ref<T extends HTMLElement> {
 
   then<R> (
     callbackFn: (value: T) => R,
-  ): RefPromise<R> {
-    this.#promise = this.#promise.then(callbackFn) as unknown as Promise<T>;
-  
-    return this as unknown as RefPromise<R>;
-  }
-
-  catch<R> (
-    callbackFn: (reason: unknown) => R,
-  ): RefPromise<R | T> {
-    this.#promise = this.#promise.then(callbackFn) as unknown as Promise<T>;
-  
-    return this as unknown as RefPromise<R | T>;
-  }
-
-  finally (
-    callbackFn: () => void,
-  ): this {
-    this.#promise.finally(callbackFn);
-  
-    return this;
+  ): Promise<R> {
+    return this.#promise.then(callbackFn);
   }
 }
